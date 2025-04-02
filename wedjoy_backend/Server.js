189797b2482ad3c983fc33dd1c63b15a2ee62912
harshpatel2405 <harshpatel4504@gmail.com>
2025-04-02 15:07:11 +0530

    Hell0

--- wedjoy_backend/Server.js
+++ wedjoy_backend/Server.js
@@ -19,7 +19,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
-      "https://wedjoyy.vercel.app",
+      "https://wedjoy.vercel.app",
     ],
     credentials: true,
   })
